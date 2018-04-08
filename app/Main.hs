e3c55755e79b4a72d3a4bfa92c435fd203d0f59e
luke <nerorevenge@gmail.com>
2018-04-07 20:14:28 -0400

    Code transfer amongst files

--- app/Main.hs
+++ app/Main.hs
@@ -1,61 +1,19 @@
 module Main where
 
-import Lib
-
-import Control.Monad ((<=<))
-import Codec.Picture
-import Graphics.Rasterific
-import Graphics.Rasterific.Texture
 import System.Environment
 
 
-fileNames :: [String]
-fileNames = ["gintoki.jpg", "gintoki2.jpg"]
+import Lib
 
 
-upperCorner :: Point  -- Point is a type synonym for V2 Float
-upperCorner = V2 0 0
+errorMessage :: String
+errorMessage = "Error: 3 args required:  mainFileName layerImageFileName outFileName "
 
 
-whiteBG ::  PixelRGBA8
-whiteBG = PixelRGBA8 255 255 255 255
-
-
-myDrawImage :: Image a -> Float -> Float -> Drawing a ()
-myDrawImage image w h = drawImageAtSize image 0 upperCorner w h
-
-
-setAlphaChannel :: Integer -> Image PixelRGBA8 -> Image PixelRGBA8
-setAlphaChannel alpha = pixelMap $ \(PixelRGBA8 x y z _) -> PixelRGBA8 x y z (fromInteger alpha)
-
-
-getPictureFiles :: [String] -> IO (Either String [DynamicImage])
-getPictureFiles =  return . sequenceA <=< (traverse readImage)  
- 
-
-makeSurePngFiles :: [DynamicImage] -> [Image PixelRGBA8]
-makeSurePngFiles = fmap convertRGBA8
-
-
-roughSketch :: [Image PixelRGBA8] -> Image PixelRGBA8
-roughSketch x = let (mainPicture:overlay:_) = x 
-                    updatedTexture = setAlphaChannel 128 overlay
-                    Image w h _  = mainPicture
-                    mw:mh:_      = fmap toEnum [w,h]
-                in renderDrawing w h whiteBG $ (myDrawImage mainPicture mw mh 
-                                               >> myDrawImage updatedTexture mw mh)
-
-realMain :: [String] -> IO ()
-realMain args = do 
-  output <- getPictureFiles . init $ args 
-  case (makeSurePngFiles <$> output) of 
-    Left msg -> putStrLn . ("Error "++) $ msg 
-    Right x -> (writePng (last args) . roughSketch) $ x
-                                                
 main :: IO ()
 main = do
   args <- getArgs
   if length args /= 3
-    then putStrLn "Error: 3 args required:  mainFileName layerImageFileName outFileName "
+    then putStrLn errorMessage
     else realMain args
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,52 @@
 module Lib
-    ( someFunc
+    ( realMain
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad ((<=<))
+import Codec.Picture
+import Graphics.Rasterific
+import Graphics.Rasterific.Texture
+
+
+fileNames :: [String]
+fileNames = ["gintoki.jpg", "gintoki2.jpg"]
+
+
+upperCorner :: Point  -- Point is a type synonym for V2 Float
+upperCorner = V2 0 0
+
+
+whiteBG ::  PixelRGBA8
+whiteBG = PixelRGBA8 255 255 255 255
+
+
+myDrawImage :: Image a -> Float -> Float -> Drawing a ()
+myDrawImage image w h = drawImageAtSize image 0 upperCorner w h
+
+
+setAlphaChannel :: Integer -> Image PixelRGBA8 -> Image PixelRGBA8
+setAlphaChannel alpha = pixelMap $ \(PixelRGBA8 x y z _) -> PixelRGBA8 x y z (fromInteger alpha)
+
+
+getPictureFiles :: [String] -> IO (Either String [DynamicImage])
+getPictureFiles =  return . sequenceA <=< (traverse readImage)  
+ 
+
+makeSurePngFiles :: [DynamicImage] -> [Image PixelRGBA8]
+makeSurePngFiles = fmap convertRGBA8
+
+
+roughSketch :: [Image PixelRGBA8] -> Image PixelRGBA8
+roughSketch x = let (mainPicture:overlay:_) = x 
+                    updatedTexture = setAlphaChannel 128 overlay
+                    Image w h _  = mainPicture
+                    mw:mh:_      = fmap toEnum [w,h]
+                in renderDrawing w h whiteBG $ (myDrawImage mainPicture mw mh 
+                                               >> myDrawImage updatedTexture mw mh)
+
+realMain :: [String] -> IO ()
+realMain args = do 
+  output <- getPictureFiles . init $ args 
+  case (makeSurePngFiles <$> output) of 
+    Left msg -> putStrLn . ("Error "++) $ msg 
+    Right x -> (writePng (last args) . roughSketch) $ x
