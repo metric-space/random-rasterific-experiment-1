da47700e40e2c81d8f0aa7650a74c83136912bb6
Metric Space <luke.meyers@synqrinus.com>
2018-05-08 19:50:50 -0400

    filtering out grey scale part of images

--- app/Main.hs
+++ app/Main.hs
@@ -7,13 +7,13 @@ import Lib
 
 
 errorMessage :: String
-errorMessage = "Error: 3 args required:  mainFileName layerImageFileName outFileName "
+errorMessage = "Error: 2 args required:  mainFileName outFileName "
 
 
 main :: IO ()
 main = do
   args <- getArgs
-  if length args /= 3
+  if length args /= 2
     then putStrLn errorMessage
     else realMain args
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,50 +3,28 @@ module Lib
     ) where
 
 import Control.Monad ((<=<))
+import Data.List
 import Codec.Picture
 import Graphics.Rasterific
 import Graphics.Rasterific.Texture
 
 
-fileNames :: [String]
-fileNames = ["gintoki.jpg", "gintoki2.jpg"]
+whiteBG ::  PixelRGB8
+whiteBG = PixelRGB8 0 0 0
 
 
-upperCorner :: Point  -- Point is a type synonym for V2 Float
-upperCorner = V2 0 0
+filterImage :: Image PixelRGB8 -> Image PixelRGB8
+filterImage = pixelMap filterImage_
 
+filterImage_ :: PixelRGB8 -> PixelRGB8
+filterImage_ pixel@(PixelRGB8 x y z) = if (length . nub $ [x,y,z]) == 1
+                                         then whiteBG
+                                         else pixel
 
-whiteBG ::  PixelRGBA8
-whiteBG = PixelRGBA8 255 255 255 255
-
-
-myDrawImage :: Image a -> Float -> Float -> Drawing a ()
-myDrawImage image = drawImageAtSize image 0 upperCorner 
-
-
-setAlphaChannel :: Integer -> Image PixelRGBA8 -> Image PixelRGBA8
-setAlphaChannel alpha = pixelMap $ \(PixelRGBA8 x y z _) -> PixelRGBA8 x y z (fromInteger alpha)
-
-
-getPictureFiles :: [String] -> IO (Either String [DynamicImage])
-getPictureFiles =  return . sequenceA <=< traverse readImage  
- 
-
-makeSurePngFiles :: [DynamicImage] -> [Image PixelRGBA8]
-makeSurePngFiles = fmap convertRGBA8
-
-
-roughSketch :: [Image PixelRGBA8] -> Image PixelRGBA8
-roughSketch x = let (mainPicture:overlay:_) = x 
-                    updatedTexture = setAlphaChannel 128 overlay
-                    Image w h _  = mainPicture
-                    mw:mh:_      = fmap toEnum [w,h]
-                in renderDrawing w h whiteBG (myDrawImage mainPicture mw mh 
-                                               >> myDrawImage updatedTexture mw mh)
 
 realMain :: [String] -> IO ()
-realMain args = do 
-  output <- getPictureFiles . init $ args 
-  case makeSurePngFiles <$> output of 
-    Left msg -> putStrLn . ("Error "++) $ msg 
-    Right x -> (writePng (last args) . roughSketch)  x
+realMain args = do
+  output <- readImage . head $ args
+  case output of
+    Left msg -> putStrLn . ("Error "++) $ msg
+    Right x -> (writePng (last args) . filterImage. convertRGB8) x
